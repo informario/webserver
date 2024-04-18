fcff3000a78bb3bee27c2af88c2be2497798137b
Hernan Lagarde <hernanmlagarde@gmail.com>
2024-04-18 06:51:47 -0300

    cambie la pc

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,13 @@ un listener tiene un iterador incoming, de varios streams
 
 
 fn main() {
-    let listener = TcpListener::bind("192.168.42.255:8996").unwrap();
+    let listener = match TcpListener::bind("192.168.0.78:8996"){
+        Ok(s) => s,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -22,15 +28,31 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
+    let request_line = buf_reader.lines().next();
+    let request_line = match request_line {
+        Some(s) => s,
+        None => return,
+    };
+    let request_line = match request_line {
+        Ok(s) => s,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match fs::read_to_string(filename){
+        Ok(s) => s,
+        Err(e) => {
+            println!("{}", e);
+            return;
+        }
+    };
     let length = contents.len();
 
     let response =
